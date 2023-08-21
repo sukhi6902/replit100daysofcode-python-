392d8a6a214982fbf6e5fc90415bb7513a6acb43
vibharao <82702791+sukhi6902@users.noreply.github.com>
2023-08-21 12:24:28 +0530

    Create day9_100dayaofcode.py

---
+++ day9_100dayaofcode.py
@@ -0,0 +1,7 @@
+score = int(input("What was your score on the test?"))
+if score >= 80:
+  print("Not too shabby")
+elif score >= 70:
+  print("Acceptable.")
+else:
+  print("Dude, you need to study more!")
